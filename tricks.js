d799ecff2b4f4da2d2235d37f4d27d6b5e680c13
Ohaluquin <luquin@gmail.com>
2023-11-10 17:15:20 -0600

    ajustes del estado

--- tricks.js
+++ tricks.js
@@ -1,5 +1,4 @@
 // JavaScript for Math Game
-
 //const timerSound = new Audio('timer-sound.mp3');
 //const correctSound = new Audio('correct-sound.mp3');
 //const incorrectSound = new Audio('incorrect-sound.mp3');
@@ -37,20 +36,14 @@ function generateQuestion() {
   // Actualizar el área de la pregunta con la nueva pregunta
   const questionArea = document.getElementById('questionArea');
   questionArea.textContent = `¿Cuánto es ${num1} + ${num2}? `;
-
-  // Guardar la respuesta correcta en una variable (podría ser una variable global)
-  currentQuestion = num1 + num2;
+  currentQuestion = num1 + num2; // Guardar la respuesta correcta en una variable
 }
 
-
 function handleInput(input) {
-  // Comprobar si la respuesta del usuario es correcta
-  if (input === currentQuestion) {
+  if (input === currentQuestion) { // Comprobar si la respuesta del usuario es correcta
     score++; // Aumentar la puntuación
     //correctSound.play();
-
-    // Opcionalmente, aumentar la dificultad
-    if (score % 3 === 0) { // cada 3 respuestas correctas
+    if (score % 3 === 0) { // cada 3 respuestas correctas aumentar la dificultad
       maxNumber += 5;
       minNumber += 3;
       if (score % 6 === 0) { // cada 10 respuestas correctas
@@ -58,51 +51,35 @@ function handleInput(input) {
       }
     }
     updateMetrics();
-
-    // Generar una nueva pregunta y reiniciar el temporizador
-    generateQuestion();
+    generateQuestion(); // Generar una nueva pregunta y reiniciar el temporizador
     remainingTime = timerDuration;
   } else {
-    // Restar una vida si la respuesta es incorrecta
-    lives--;
+    lives--; // Restar una vida si la respuesta es incorrecta
     //incorrectSound.play();
     updateMetrics();
-
-    // Comprobar si quedan vidas
-    if (lives <= 0) {
-      // Terminar el juego si no quedan vidas
-      endGame();
-    } else {
-      // Opcional: Generar una nueva pregunta y reiniciar el temporizador
+    if (lives <= 0) { // Comprobar si quedan vidas
+      endGame(); // Terminar el juego si no quedan vidas
+    } else { // Opcional: Generar una nueva pregunta y reiniciar el temporizador
       generateQuestion();
       remainingTime = timerDuration;
     }
   }
 }
 
-function updateTimer() {
-  // Actualizar el área del temporizador
+function updateTimer() {  
   const timerArea = document.getElementById('timerArea');
-  timerArea.textContent = `Tiempo restante: ${remainingTime}s`;
-
-  // Verificar si el tiempo se ha agotado
-  if (remainingTime <= 0) {
-    // Restar una vida
-    lives--;
+  timerArea.textContent = `Tiempo restante: ${remainingTime}s`; // Actualizar el área del temporizador
+  if (remainingTime <= 0) { // Verificar si el tiempo se ha agotado
+    lives--; // Restar una vida
     updateMetrics();    
-
-    // Comprobar si quedan vidas
-    if (lives <= 0) {
-      // Terminar el juego si no quedan vidas
-      endGame();
-    } else {
-      // Generar una nueva pregunta y reiniciar el temporizador
+    if (lives <= 0) { // Comprobar si quedan vidas
+      endGame(); // Terminar el juego si no quedan vidas
+    } else { // Generar una nueva pregunta y reiniciar el temporizador
       generateQuestion();
       remainingTime = timerDuration;
     }
   }
-  // Disminuir el tiempo restante
-  remainingTime--;
+  remainingTime--; // Disminuir el tiempo restante
   //timerSound.play();
 }
 
@@ -114,16 +91,13 @@ function startGame() {
   minNumber = 1;
   timerDuration = 12;
   remainingTime = timerDuration;
-
   // Limpiar los mensajes y el área de estado
   const statusArea = document.getElementById('statusArea');
   statusArea.innerHTML = "";
   const userInput = document.getElementById('userInput');
   userInput.value = "";
-
   // Iniciar el temporizador
   timer = setInterval(updateTimer, 1000); // Llama a updateTimer cada segundo
-
   // Generar la primera pregunta
   generateQuestion();
 }
@@ -131,15 +105,10 @@ function startGame() {
 
 
 function endGame() {
-  // Detener el temporizador
-  clearInterval(timer);
-
-  // Mostrar el puntaje final
-  const statusArea = document.getElementById('statusArea');
+  clearInterval(timer); // Detener el temporizador
+  const statusArea = document.getElementById('statusArea');  // Mostrar el puntaje final
   if(score>=0) statusArea.innerHTML = `Juego terminado. Tu puntuación final es: ${score}`;
-
-  // Opcionales: Mostrar mensajes según el desempeño, botón de reinicio, etc.
-  if (score <0) {
+  if (score <0) { // Opcionales: Mostrar mensajes según el desempeño, botón de reinicio, etc.
     statusArea.innerHTML += '<br>¡Presiona el botón para iniciar!';
   } else if(score < 15) {
     statusArea.innerHTML += '<br>¡Sigue practicando!';
@@ -148,15 +117,13 @@ function endGame() {
   } else {
     statusArea.innerHTML += '<br>¡Buen trabajo!';
   }
-
-  // Botón de reinicio
-  if(score>=0) {
+  if(score>=0) {  // Botón de reinicio
     statusArea.innerHTML += '<br><button onclick="startGame()">Jugar de nuevo</button>';
     // Mostrar el mensaje de fin de juego
     var randomTipIndex = Math.floor(Math.random() * tipsForImprovement.length);
     var tipToShow = tipsForImprovement[randomTipIndex];
     // Actualizar el contenedor del mensaje de fin de juego y mostrarlo
-    statusArea.innerHTML += "<p>Consejo para mejorar: " + tipToShow + "</p>";
+    statusArea.innerHTML += "<p>Consejo para mejorar:<br> " + tipToShow + "</p>";
     }
   else statusArea.innerHTML += '<br><button onclick="startGame()">Jugar</button>';
 }
@@ -165,23 +132,17 @@ function endGame() {
 let currentInput = ""; // Variable global para almacenar la entrada del usuario
 
 function appendToInput(value) {
-  // Añadir el valor al input actual
-  currentInput += value;
-
-  // Actualizar el campo de entrada en el HTML
-  const userInput = document.getElementById('userInput');
+  if(score<0 || lives<=0) return;
+  currentInput += value; // Añadir el valor al input actual
+  const userInput = document.getElementById('userInput'); // Actualizar el campo de entrada en el HTML
   userInput.value = currentInput;
 }
 
 function submitInput() {
-  // Convertir la entrada del usuario a un número
-  const numericalInput = parseFloat(currentInput);
-
-  // Pasar la entrada a handleInput para su verificación
-  handleInput(numericalInput);
-
-  // Limpiar el campo de entrada para la próxima pregunta
-  currentInput = "";
+  if(score<0 || lives<=0) return;
+  const numericalInput = parseFloat(currentInput); // Convertir la entrada del usuario a un número
+  handleInput(numericalInput); // Pasar la entrada a handleInput para su verificación
+  currentInput = ""; // Limpiar el campo de entrada para la próxima pregunta
   const userInput = document.getElementById('userInput');
   userInput.value = currentInput;
 }
@@ -190,7 +151,6 @@ function submitInput() {
 function updateMetrics() {
   const scoreArea = document.getElementById('scoreArea');
   scoreArea.textContent = `Puntuación: ${score}`;
-
   const difficultyArea = document.getElementById('difficultyArea');
   let levelImage = document.getElementById('levelImage');
   if (score <= 10) {
@@ -209,24 +169,20 @@ function updateMetrics() {
     difficultyArea.textContent = "Experto";
     levelImage.src = 'experto.png';
   }
-
   const livesArea = document.getElementById('livesArea');
   livesArea.textContent = `Vidas: ${lives}`;
 }
 
 function deleteLastInput() {
-  // Eliminar el último carácter de la entrada actual
-  currentInput = currentInput.slice(0, -1);
-
-  // Actualizar el campo de entrada en el HTML
-  const userInput = document.getElementById('userInput');
+  currentInput = currentInput.slice(0, -1); // Eliminar el último carácter de la entrada actual
+  const userInput = document.getElementById('userInput'); // Actualizar el campo de entrada en el HTML
   userInput.value = currentInput;
 }
 
 // Lista de consejos para mejorar en sumas
 const tipsForImprovement = [
   "Recuerda que sumar de cinco en cinco puede simplificar cómo haces tus cálculos.",
-  "Redondea y ajusta: Intenta redondear el número más grande al múltiplo de diez más cercano y ajusta después.",
+  "Redondea y ajusta: Intenta redondear el número más grande al múltiplo de diez más cercano y ajusta después. Por ejemplo, 68+23 se parece a 70+23=93 y ajustas a 91",
   "Complementos: Identifica los complementos de cinco y diez, son muy útiles para unir los números como si fueran rompecabezas.",
   "Duplicar: Practica duplicar cualquier número, te puede facilitar los calculos.",
   "Conmutativa: Si eliges primero el número mayor y luego añades el menor, es más rápido el cálculo.",
